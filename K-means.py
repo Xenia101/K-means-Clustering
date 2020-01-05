7fc1e036fc5ad301dc68381e57cf2f02b28a0a09
XENIA <lommy101@gmail.com>
2020-01-06 00:21:20 +0900

    Update K-means.py

--- K-means.py
+++ K-means.py
@@ -14,3 +14,8 @@ kmeams = KMeans(n_clusters = 3, n_jobs = 4, random_state=21)
 kmeams.fit(X)
 
 print(kmeams.cluster_centers_)
+
+new_y = kmeams.labels_
+
+plt.scatter(X[:, 0], X[:, 1], c=new_y)
+plt.show()
